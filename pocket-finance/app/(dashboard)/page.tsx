f510b2ce5214593f77723ec2d85c13497f4502cb
jacob-gilbert <jacob.gilbert@richmond.edu>
2024-06-07 22:45:29 -0400

    authentication working

--- pocket-finance/app/(dashboard)/page.tsx
+++ pocket-finance/app/(dashboard)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <p>
-      This is an authenticated route
+      <UserButton afterSignOutUrl="/"/>
     </p>
   )
 }
