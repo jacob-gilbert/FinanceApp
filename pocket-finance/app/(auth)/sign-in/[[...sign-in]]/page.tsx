c6f9d77a4e30043e0f43d0fcbe24ca8b7d10d500
jacob-gilbert <jacob.gilbert@richmond.edu>
2024-06-07 22:00:16 -0400

    auth now forces to sign in or sign up pages

--- pocket-finance/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ pocket-finance/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,5 @@
-const SignInPage = () => {
-    return (
-        <div>
-            Sign In page
-        </div>
-    );
-};
+import { SignIn } from "@clerk/nextjs";
 
-export default SignInPage;
+export default function Page() {
+  return <SignIn path="/sign-in" />;
+}
