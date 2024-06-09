61249d873c7aa8e50d2e51428c3b4e83dec6a2ab
jacob-gilbert <jacob.gilbert@richmond.edu>
2024-06-09 19:56:39 -0400

    hono component added

---
+++ pocket-finance/app/api/[[...route]]/route.ts
@@ -0,0 +1,18 @@
+// hono overwrites existing nextjs route handlers so we
+// do not have to do file based routing
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel';
+
+import authors from "./authors"
+import books from "./books"
+
+export const runtime = 'edge';
+
+const app = new Hono().basePath('/api')
+
+app.route("/authors", authors);
+app.route("/books", books);
+  
+
+export const GET = handle(app)
+export const POST = handle(app)
